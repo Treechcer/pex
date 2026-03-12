de2562428e6b16c48ad632077ebcaa0790a664b6
treecher <treechcer@gmail.com>
2026-03-12 22:18:17 +0100

    now

--- source/game.lua
+++ source/game.lua
@@ -223,7 +223,7 @@ function game.functions.checkIfWin()
     local winTab = game.winCond
     if winTab.found >= winTab.haveToFind then
         winTab.won = true
-        game.functions.createButton(game.width / 2 - 125, game.height / 4 - 50, 48, 48, game.functions.restartGame, "card_face_down", {state = "button_end"})
+        --game.functions.createButton(game.width / 2 - 125, game.height / 4 - 50, 48, 48, game.functions.restartGame, "card_face_down", {state = "button_end"})
         game.functions.endScreen()
     end
 end
